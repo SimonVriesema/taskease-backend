9ed786d3897be4bd410b34734d2254a215582a3a
Simon Vriesema <simon.vriesema@ah.nl>
2024-04-08 19:47:34 +0200

    users can now be saved to the h2 database

--- src/main/java/com/taskease/taskeasebackend/models/User.java
+++ src/main/java/com/taskease/taskeasebackend/models/User.java
@@ -1,49 +1,28 @@
 package com.taskease.taskeasebackend.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "app_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String email;
     private String firstName;
     private String preposition;
     private String lastName;
     private String description;
-    @Column(name = "date_of_birth")
     private String dateOfBirth;
-
-    @Column(name = "job_title")
     private String jobTitle;
-
-    public User() {
-    }
-
-    public User(String email, String firstName, String preposition, String lastName, String description, String dateOfBirth, String jobTitle) {
-        this.email = email;
-        this.firstName = firstName;
-        this.preposition = preposition;
-        this.lastName = lastName;
-        this.description = description;
-        this.dateOfBirth = dateOfBirth;
-        this.jobTitle = jobTitle;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", email='" + email + '\'' +
-                ", preposition='" + preposition + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", description='" + description + '\'' +
-                ", dateOfBirth='" + dateOfBirth + '\'' +
-                ", jobTitle='" + jobTitle + '\'' +
-                '}';
-    }
 }
 
