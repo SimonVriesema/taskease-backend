08952bb34c685a6241af0a945e1956e933516846
Simon Vriesema <simon.vriesema@ah.nl>
2024-09-01 14:14:10 +0200

    Create UserRepository functions

--- src/main/java/com/taskease/taskeasebackend/controllers/UserController.java
+++ src/main/java/com/taskease/taskeasebackend/controllers/UserController.java
@@ -5,10 +5,15 @@ import com.taskease.taskeasebackend.services.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
--- src/main/java/com/taskease/taskeasebackend/repositories/UserRepository.java
+++ src/main/java/com/taskease/taskeasebackend/repositories/UserRepository.java
@@ -4,12 +4,35 @@ import com.taskease.taskeasebackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Override
     <S extends User> S save(S user);
+
     @Override
-    Optional<User> findById(Long aLong);
+    Optional<User> findById(Long id);
+
+    //TODO: Create Endpoint
+    @Override
+    List<User> findAll();
+
+    //TODO: Create Endpoint
+    @Override
+    void delete(User user);
+
+    //TODO: Create Endpoint
+    @Override
+    void deleteById(Long id);
+
+    //TODO: Create Endpoint
+    Optional<User> findByUsername(String username);
+
+    //TODO: Create Endpoint
+    Optional<User> findByEmail(String email);
+
+    //TODO: Create Endpoint
+    boolean existsByEmail(String email);
 }
